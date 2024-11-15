81c0814d89cbb27c8443a5f567901320bdede224
5216181 <51808709+5216181@users.noreply.github.com>
2024-11-15 16:39:41 +0800

    Add files via upload

---
+++ toolbox.js
@@ -0,0 +1,11 @@
+'use strict';
+
+if (typeof toolbox_custom == 'undefined')
+  var toolbox_custom = [];
+
+toolbox_custom.push(''
++'<category id="category_custom" name="miniTomas" colour="100">'
++'<block type="pinmode1114"/>'
++'<block type="encodeMotor_type2"/>'
++'<block type="encodeMotor"/>'
++'</category>');
