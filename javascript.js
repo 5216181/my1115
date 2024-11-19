daef82ae503cb0378a05cd1cdab96c8b858e8207
5216181 <51808709+5216181@users.noreply.github.com>
2024-11-19 17:53:26 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ Blockly.Arduino['pinmode1114'] = function(block) {
   var value_i1 = Blockly.Arduino.valueToCode(block, 'i1', Blockly.Arduino.ORDER_ATOMIC);
   var dropdown_i2 = block.getFieldValue('i2');
   // TODO: Assemble Arduino into code variable.
-  var code = '...;\n';
+  var code = 'p1...;\n';
   return code;
 };
 
@@ -12,7 +12,7 @@ Blockly.Arduino['encodemotor'] = function(block) {
   var value_e3 = Blockly.Arduino.valueToCode(block, 'e3', Blockly.Arduino.ORDER_ATOMIC);
   var value_e4 = Blockly.Arduino.valueToCode(block, 'e4', Blockly.Arduino.ORDER_ATOMIC);
   // TODO: Assemble Arduino into code variable.
-  var code = '...;\n';
+  var code = 'p2...;\n';
   return code;
 };
 
@@ -21,6 +21,6 @@ Blockly.Arduino['encodemotor_type2'] = function(block) {
   var value_e3 = Blockly.Arduino.valueToCode(block, 'e3', Blockly.Arduino.ORDER_ATOMIC);
   var value_e4 = Blockly.Arduino.valueToCode(block, 'e4', Blockly.Arduino.ORDER_ATOMIC);
   // TODO: Assemble Arduino into code variable.
-  var code = '...;\n';
+  var code = 'p3...;\n';
   return code;
 };
